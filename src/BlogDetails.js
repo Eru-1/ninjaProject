48e61379bd5c41054eaced1b1dea3ab20fed6b65
Sergey <s.alyasev@bk.ru>
2023-06-19 16:51:16 +0300

    final commit

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,11 +1,32 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
+import { useNavigate } from "react-router-dom";
 
 const BlogDetails = () => {
     const { id } = useParams();
+    const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
+    const moveBack = useNavigate();
+
+    const handleClick = () => {
+        fetch('http://localhost:8000/blogs/' + blog.id, {
+            method: 'DELETE'
+        }).then (() => {
+            moveBack('/')
+        })
+    }
 
     return (
         <div className="blog-details">
-            <h2>Blog details - { id }</h2>
+            { isPending && <div>Loading...</div> }
+            { error && <div>{ error }</div> }
+            { blog && (
+                <article>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by { blog.author }</p>
+                    <div>{ blog.body }</div>
+                    <button onClick={handleClick}>delete</button>
+                </article>
+            )}
         </div>
     );
 }
